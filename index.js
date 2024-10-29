b86213fd8b76cef88e2e078fa06c399114d77800
Rahul Singh <chauhan.rahul2605@gmail.com>
2024-10-29 18:14:57 +0000

    corrected the event obj

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.post('/azure-event', (req, res) => {
     console.log("Request body is: ", req.body)
 
-    const event = req.body;
+    const event = req.body[0];
     if(event.eventType === "Microsoft.EventGrid.SubscriptionValidationEvent"){
         const response = {
             validationResponse: event.data.validationCode
@@ -14,7 +14,7 @@ app.post('/azure-event', (req, res) => {
         console.log("Reponding back with:", response);
         res.status(200).json(response)
     } else {
-        console.log("Received event", event);
+        console.log("Received request with body", req.body);
         res.status(200).send("Got it!");
     }
 })
