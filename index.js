e54b4b16f3441f1fe20d1d5eec8063e2aa2fcf7b
Rahul Singh <chauhan.rahul2605@gmail.com>
2024-10-29 17:43:03 +0000

    Init endpoint

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express()
+app.use(express.json());
+
+app.post('/azure-event', (req, res) => {
+    console.log("Request body is: ", req.body)
+
+    const event = req.body;
+    if(event.eventType === "Microsoft.EventGrid.SubscriptionValidationEvent"){
+        const response = {
+            validationResponse: event.data.validationCode
+        }
+        res.status(200).send(response)
+    } else {
+        console.log("Received event", event);
+        res.status(200).send("Got it!");
+    }
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log("Listening on port: ", PORT)
+} )
